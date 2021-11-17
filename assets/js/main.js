435e4381de8bace197e9574a0bf30733e34e27cb
munozsolar <64947139+munozsolar@users.noreply.github.com>
2021-11-16 22:47:11 -0300

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
-function bienvenida() {
-    alert("Bienvenido a la pagina web!")
+
+const body=document.querySelector('body')
+
+const colores=['blue','yellow','green','black','white']
+
+
+
+function fondo(){
+    var indice=parseInt(Math.random()*colores.length);
+    body.style.backgroundColor=colores[indice]
+    
 }
+
+
